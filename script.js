04289c53ba2ad319fb711270f34367afcffc1e60
Rafiullah-Najat <rafiullahnajat2026@gmail.com>
2026-09-05 23:06:22 -0700

    Fix: images references

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-const li = document.querySelectorAll('.menu ul li');
-
+const li = document.querySelectorAll(".menu ul li");
 
 li.forEach((item) => {
-    item.addEventListener('click',()=>{
+  item.addEventListener("click", () => {
+    li.forEach((v) => {
+      v.classList.remove("active");
+    });
 
-        li.forEach((v)=>{
-            v.classList.remove('active');
-        })
-
-        item.classList.add('active')
-
-    })
-})
+    item.classList.add("active");
+  });
+});
